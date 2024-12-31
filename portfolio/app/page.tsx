dedb1d94eff8f047ceb859129a60bf1cdc7f4666
Daniel Okello <43291086+okellodaniel@users.noreply.github.com>
2024-12-31 16:35:26 +0300

    removed unused import

--- portfolio/app/page.tsx
+++ portfolio/app/page.tsx
@@ -2,7 +2,6 @@
 
 import { useState } from 'react'
 import { motion } from 'framer-motion'
-import Link from 'next/link'
 import Header from '../components/Header'
 import Hero from '../components/Hero'
 import About from '../components/About'
